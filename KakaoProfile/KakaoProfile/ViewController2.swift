9ef592e3bb2f98d8c1ba1e818e8a5c1c55b032b8
harrison.tom <harrison.tom@kakaocorp.com>
2021-01-11 20:44:12 +0900

    프로젝트 생성, Initial ViewController (TabBar Controller) 생성

---
+++ KakaoProfile/KakaoProfile/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  KaKaoProfile
+//
+//  Created by 이준형 on 2021/01/11.
+//  Copyright © 2021 이준형. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Item2 :", #file, #line, #function, #column)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # swift-w2-profile
 모바일 2주차 프로필 저장소
+
+# 2021.01.11
+1. TabBar Controller 추가 및 Initial ViewContoller로 지정 - Bar Title -> Item1
+2. 또 다른 View Controller 생성 후 Bar Title -> Item2
+3. 각 View Controller에 class 할당
+4. 각 viewDidLoad() function에서 "print(#file, #line, #function, #column)" 출력
+	-> Item1 : /Users/harrison.tom/Desktop/xcode/210111/swift-w2-profile/KaKaoProfile/KaKaoProfile/ViewController.swift 15 viewDidLoad() 51
+	-> Item2 : /Users/harrison.tom/Desktop/xcode/210111/swift-w2-profile/KaKaoProfile/KaKaoProfile/ViewController2.swift 15 viewDidLoad() 51
+
