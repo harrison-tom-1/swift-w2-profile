91b5b9791b06785d85c8d3ca1aef7b76caea115c
harrison.tom <harrison.tom@kakaocorp.com>
2021-01-13 18:23:25 +0900

    공백 및 불필요한 함수 제거

---
+++ KakaoProfile/KakaoProfile/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  KaKaoProfile
+//
+//  Created by 이준형 on 2021/01/12.
+//  Copyright © 2021 이준형. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
